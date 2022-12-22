d96ce968a00628fa6270282b1f205fc03d326b10
Emilia <emilia.a.wis@gmail.com>
2022-12-22 09:34:59 +0100

    sprawdzanie czy koniec czesc 2
    poprawienie błędu (for szedł od 1 zamiast od 0) oraz dodanie sprawdzenia czy są 4 w rzędzie
    poprawienie opisow w komentarzach

--- Quarto/quarto.rs
+++ Quarto/quarto.rs
@@ -22,16 +22,45 @@ fn check_if_end(tab: &[[i8;4];4], player: i8) -> bool{
 		return true;
 	}
 	
-	//jesli sa wolne miejsca sprawdzam czy sa 4 w rzedzie
+	//skoro sa jeszcze wolnie miejsca sprawdzam czy nie ma 4 takich samych w rzedzie / kolumnie / na skosach
 	
 	let feature = [2,3,5,7];//mod wlasciwosci
 	let mut count = 0;
 	let mut free_elements = 0;
 	
+	//sprawdzam czy sa 4 w kolumnie
+	
 	for f in feature {//dla kazdej wlasciwosci
 		for i in 0..4 { //dla kazdej kolumny
 			//sprawdzenie wlasnosci dla wszystkich elementow
-			for j in 1..4 {
+			for j in 0..4 {
+				if tab[i][j] % f == 0{//jesli jest spelnione 
+					count += 1;
+				}
+				if tab[i][j] == 0{//jesli jest puste
+					free_elements += 1;
+				}
+			}
+			
+			//sprawdzenie czy sa 4 w kolumnie
+			if free_elements == 0{//jesli wszystkie uzupelnione
+				if count == 0 || count == 4{//jesli wszystkie takie same
+					//koniec gry
+					print_end(player);
+					return true;
+				}
+			}
+		}
+		count = 0;
+		free_elements = 0;
+	}
+	
+	//4 w rzędzie
+	
+	for f in feature {//dla kazdej wlasciwosci
+		for j in 0..4 { //dla kazdego rzedu
+			//sprawdzenie wlasnosci dla wszystkich elementow
+			for i in 0..4 {
 				if tab[i][j] % f == 0{//jesli jest spelnione 
 					count += 1;
 				}
@@ -52,12 +81,12 @@ fn check_if_end(tab: &[[i8;4];4], player: i8) -> bool{
 		count = 0;
 		free_elements = 0;
 	}
+	
 	false
 }
 
 fn main(){
 	let tab: [[i8;4];4] = [[0;4];4];//utworzenie tablicy w ktorej bedzie zapisywany stan gry
 	//jesli 0 to puste pole, w innym przypadkach odpowiednio 1*(2)*(3)*(5)*(7) zaleznie od cech elementu 
-	let mut player = 0;//czyj jest ruch
-	println!("{}",check_if_end(&tab,player));
+	println!("{}", check_if_end(&tab, 0));
 }
