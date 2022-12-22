725b4b78434ca365ff5bc500ae446c09ab08e8f0
Emilia <emilia.a.wis@gmail.com>
2022-12-22 09:22:52 +0100

    sprawdzanie czy koniec gry - czesc 1
    program sprawdza czy nie jest cała plansza zapełniona lub czy nie ma 4 z jedną własnością w jednej kolumnie

--- Quarto/quarto.rs
+++ Quarto/quarto.rs
@@ -1,6 +1,63 @@
 //quarto
 
+fn print_end(player: i8){
+	println!("Wygral gracz {}", player + 1);
+}
+
+fn check_if_end(tab: &[[i8;4];4], player: i8) -> bool{
+	
+	//sprawdzam czy sa wolne miejsca
+	let free = 0;//co jest jesli miejsce jest wolne
+	let mut sum = 0;
+	for i in 0..4{
+		for j in 0..4{
+			if tab[i][j] == free{//
+				sum += 1;//licze sume wolnych miejsc
+			}
+		}
+	}
+	
+	if sum == 0{//brak wolnych miejsc
+		print_end(player);
+		return true;
+	}
+	
+	//jesli sa wolne miejsca sprawdzam czy sa 4 w rzedzie
+	
+	let feature = [2,3,5,7];//mod wlasciwosci
+	let mut count = 0;
+	let mut free_elements = 0;
+	
+	for f in feature {//dla kazdej wlasciwosci
+		for i in 0..4 { //dla kazdej kolumny
+			//sprawdzenie wlasnosci dla wszystkich elementow
+			for j in 1..4 {
+				if tab[i][j] % f == 0{//jesli jest spelnione 
+					count += 1;
+				}
+				if tab[i][j] == 0{//jesli jest puste
+					free_elements += 1;
+				}
+			}
+			
+			//sprawdzenie czy sa 4 w rzedzie
+			if free_elements == 0{//jesli wszystkie uzupelnione
+				if count == 0 || count == 4{//jesli wszystkie takie same
+					//koniec gry
+					print_end(player);
+					return true;
+				}
+			}
+		}
+		count = 0;
+		free_elements = 0;
+	}
+	false
+}
+
 fn main(){
 	let tab: [[i8;4];4] = [[0;4];4];//utworzenie tablicy w ktorej bedzie zapisywany stan gry
 	//jesli 0 to puste pole, w innym przypadkach odpowiednio 1*(2)*(3)*(5)*(7) zaleznie od cech elementu 
+	let mut player = 0;//czyj jest ruch
+	println!("{}",check_if_end(&tab,player));
 }
