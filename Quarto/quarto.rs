c26d0df8f787a976790ee674db09595b0ab3fda8
Emilia <emilia.a.wis@gmail.com>
2022-12-22 08:42:59 +0100

    utworzenie poczatku kodu
    stworzenie funkcji main oraz tablicy w ktorej beda przechowywane elementy

---
+++ Quarto/quarto.rs
@@ -0,0 +1,6 @@
+//quarto
+
+fn main(){
+	let tab: [[i8;4];4] = [[0;4];4];//utworzenie tablicy w ktorej bedzie zapisywany stan gry
+	//jesli 0 to puste pole, w innym przypadkach odpowiednio 1*(2)*(3)*(5)*(7) zaleznie od cech elementu 
+}
