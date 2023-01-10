6cec8e28755526ae6c4c25d992d6feac2d57bb30
Jakub <koconjakub831@gmail.com>
2023-01-10 17:36:47 +0100

    czytanie zmiennych

--- Quarto/quarto.rs
+++ Quarto/quarto.rs
@@ -1,4 +1,14 @@
 //quarto
+fn place_on_board(mut tab: [[i32;4];4], element: i32) -> [[i32; 4]; 4]
+{ 
+	loop { //pętla w nieskończoność do podania niezajętego miejsca  
+        let mut line = String::new(); //wczytywanie user inputu             
+        io::stdin().read_line(&mut line).unwrap();
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        let a: usize = parts[0].parse().unwrap();
+        let b: usize = parts[1].parse().unwrap();
+    } 
+}
 
 fn main(){
 	let tab: [[i32;4];4] = [[0;4];4];//utworzenie tablicy w ktorej bedzie zapisywany stan gry
